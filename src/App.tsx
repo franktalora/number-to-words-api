8fa69537248f536ec5e1ae25d327e0ae5d45bbb4
franktalora <hello@franktalora.com>
2024-09-17 14:12:21 +1000

    Since I don't have Visual Studio installed atm to hot-reload/debug these C# functions, I'm going to do it on the front-end.  Added a node script to run the .NET backend and the Vite frontend server concurrently.  Updated the UI so we can test these numbers.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,47 @@
-import { useState } from 'react'
+import { useState } from "react";
+
+const handleConvert = async (numberToConvert: string) => {
+  if (!numberToConvert) {
+    throw new Error("Please enter a number");
+  }
+
+  const response = await fetch(
+    `http://localhost:5290/number-to-words/${numberToConvert}`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to convert number");
+  }
+  const data: { words: string; number: string } = await response.json();
+
+  return data;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [number, setNumber] = useState("");
+
+  const getConvertedNumber = async () => {
+    const convertedData = await handleConvert(number);
+    console.log("convertedData", convertedData)
+    return convertedData
+  };
 
   return (
     <>
       <h1>Vite + React</h1>
       <div className="p-2">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <input
+          type="number"
+          placeholder="Enter a number"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+        <button onClick={getConvertedNumber}>Convert</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
