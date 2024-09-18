d8ff2849b349b7a7d83b20cab7f93175fa1d3d1a
franktalora <hello@franktalora.com>
2024-09-18 20:32:29 +1000

    Added CORS config to backend to fix (hopefully) access-control-allow-origin issue that was happening on the frontend

--- @number-to-words-api/Program.cs
+++ @number-to-words-api/Program.cs
@@ -5,9 +5,18 @@ public class Program
   public static void Main(string[] args)
   {
     var builder = WebApplication.CreateBuilder(args);
+    builder.Services.AddCors();
+
     var app = builder.Build();
 
     app.UseHttpsRedirection();
+
+    // Configure CORS to work with Vite/React (Vite's default port is 5173)
+    app.UseCors(policy => policy
+      .WithOrigins("http://localhost:5173")
+      .AllowAnyMethod()
+      .AllowAnyHeader()
+    );
 
     app.MapGet("/number-to-words-api/convert-price/{number}", (double number) =>
     {
